f172e5ba3851b5d8403f8b60bd1d9bbf99b7d68b
keensteven <keenstevie@gmail.com>
2019-07-30 10:46:23 -0400

    working with renamed db and incorporated some MVC code

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,15 @@
 // Import MySQL connection.
 
-var connection = require("../config/connection.js");
+// var connection = require("../config/connection.js");
 
-
+// var orm = {
+//     all: function(tableInput, cb){
+//         connection.query('SELECT * FROM '+tableInput+';', function(err,result){
+//             if(err) throw err;
+//             cb(result)
+//         })
+//     }
+// }
 
 // Export the orm object for the model (cat.js).
-module.exports = orm;
+// module.exports = orm;
